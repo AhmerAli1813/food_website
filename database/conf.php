ab6e22a94c62c1581457dce77b9bfeef69d66a8c
AHMER ALI <98941104+AhmerAli1813@users.noreply.github.com>
2022-10-12 02:55:11 -0700

    change in conf.php file

--- database/conf.php
+++ database/conf.php
@@ -1,8 +1,8 @@
 <?php 
 
-$server = "sql213.epizy.com";
-$username = "epiz_32582462";
-$password = "dhEUbDue2fT39N";
+$server = "localhost";
+$username = "root";
+$password = "";
 $database = "epiz_32582462_food_website";
 
 $conn = mysqli_connect($server , $username ,$password ,$database) or die("database connection failed");
