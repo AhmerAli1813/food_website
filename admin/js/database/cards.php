172f13dcca594500b20ec1b4d1fd394134a1cf12
AHMER ALI <98941104+AhmerAli1813@users.noreply.github.com>
2023-01-25 18:34:40 +0500

    cards of dashboard completed

--- admin/js/database/cards.php
+++ admin/js/database/cards.php
@@ -1,12 +1,15 @@
 <?php
 include "../../../database/conf.php";
  $f = [];
+ $output = "";
 $q = $conn->query("SELECT COUNT(*) as user FROM `register` ");
 if($q){
         $row = mysqli_fetch_assoc($q);
         $res = [
             "name" => "all user",
-            "result" => $row["user"]
+            "result" => $row["user"],
+            "color" => "var(--bs-danger)",
+            "icons" => "fas fa-user"
         ];
         // array_push
       $f[] = $res;
@@ -16,39 +19,64 @@ if($q2){
     $row = mysqli_fetch_assoc($q2);
     $res = [
         "name" => "all products",
-        "result" => $row["pro"]
+        "result" => $row["pro"],
+        "color" => "var(--bs-info)",
+        "icons" => "fas fa-cart-arrow-down"
     ];
     $f[] = $res;
 
 
 }
 
-echo "<pre>";
-echo print_r($f);
-echo "</pre>";
-foreach ($f as $row){
-    
-echo "<pre>";
-echo print_r($row["result"]);
-echo "</pre>";
+$q3=$conn->query("SELECT COUNT(*) as cat FROM `catagory` ");
+if($q3){
+    $row = mysqli_fetch_assoc($q3);
+    $res = [
+        "name" => "all category",
+        "result" => $row["cat"],
+        "color" => "var(--first-color)",
+        "icons" => "fas fa-utensils"
+    ];
+    $f[] = $res;
+
+
 }
-// echo json_encode(["data"=>$data] , true);
+
+$q4=$conn->query("SELECT COUNT(*) as pro FROM `card`");
+if($q4){
+    $row = mysqli_fetch_assoc($q4);
+    $res = [
+        "name" => "all cards",
+        "result" => $row["pro"],
+        "color" => "var(--bs-success)",
+        "icons" => "fas fa-cart-arrow-down"
+    ];
+    $f[] = $res;
 
 
-// $output.='<div class="col-xl-3 col-md-6 mb-4">
-//     <div class="card border-left shadow h-100 py-2" style="--i:var(--bs-success)">
-//         <div class="card-body">
-//             <div class="row no-gutters align-items-center">
-//                 <div class="col mr-2">
-//                     <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-//                                 All User    
-//                     </div>
-//                     <div class="h5 mb-0 font-weight-bold text-gray-800">'.$row["user"].'</div>
-//                 </div>
-//                 <div class="col-auto">
-//                     <i class="fas fa-dollar-sign fa-2x "></i>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//   </div>';
+}
+
+foreach ($f as $row){
+
+$output.='<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left shadow h-100 py-2" style="--i:'.$row["color"].'   ">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            '.$row["name"].'    
+                    </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">'.$row["result"].'</div>
+                </div>
+                <div class="col-auto">
+                    <i class="'.$row["icons"].' fa-2x "></i>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>';
+
+
+}
+echo json_encode(["data"=>$output] , true);
+
---
+++ admin/js/database/n.php
@@ -0,0 +1,7 @@
+<?php 
+
+$a = ["apple" , "cat"];
+
+// echo "<pre>";
+echo print_r($a);
+// echo "</pre>";
